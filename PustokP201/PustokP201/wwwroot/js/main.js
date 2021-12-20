9415634f7b43f93aaea901bd9b7588862c5bb203
EAghayev <eaghayevv@gmail.com>
2021-12-20 18:06:56 +0400

    project added

---
+++ PustokP201/PustokP201/wwwroot/js/main.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+
+    $(document).on("click", ".single-eye-btn", function () {
+
+        let id = $(this).attr("data-id");
+
+        fetch(`/home/getbook/${id}`)
+            //.then(response => response.json())
+            .then(response => response.text())
+            .then(data => {
+
+                console.log(data)
+                $("#detailModal .modal-content").html(data)
+              /*  $("#detailModal").find(".product-title").text(data.name)*/
+            })
+            
+      
+
+        $("#detailModal").modal("show")
+    })
+})
